5903b2880ea61bbf476b76cd9dfb7db3f33f52e7
maangie <maangie906@gmail.com>
2016-02-07 16:25:02 +0900

    show items of roots in header menu.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'categories#show', id: 1
   resources :categories
   # For details on the DSL available within this file, see
   # http://guides.rubyonrails.org/routing.html
