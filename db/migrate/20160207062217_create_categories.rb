7533a28ec1e2b43f7f88468acb9653a160bba20a
maangie <maangie906@gmail.com>
2016-02-07 15:24:12 +0900

    tweak a migration file.

--- db/migrate/20160207062217_create_categories.rb
+++ db/migrate/20160207062217_create_categories.rb
@@ -1,10 +1,10 @@
 class CreateCategories < ActiveRecord::Migration[5.0]
   def change
     create_table :categories do |t|
-      t.string :label
-      t.integer :parent_id
-      t.integer :lft
-      t.integer :rgt
+      t.string :label, null: false
+      t.integer :parent_id, null: true, index: true
+      t.integer :lft, null: false, index: true
+      t.integer :rgt, null: false, index: true
 
       t.timestamps
     end
